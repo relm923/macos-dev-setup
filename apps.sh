cb1132ce9f4bd3c6b8fe817bc3ac26bbeb24dc21
Reagan Elm <reagan.elm@gmail.com>
2021-01-06 11:52:46 -0500

    add apps

---
+++ apps.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+source ./utils.sh
+
+brew install --cask \
+    1password \
+    alfred \
+    docker \
+    google-chrome \
+    postman \
+    slack \
+    spotify \
+    visual-studio-code \
