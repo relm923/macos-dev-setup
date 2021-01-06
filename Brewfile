b60dc1f72b0e71f3c30be120fee44ee0b1a301a8
Reagan Elm <reagan.elm@gmail.com>
2021-01-06 12:25:00 -0500

    convert to Brewfile

---
+++ Brewfile
@@ -0,0 +1,23 @@
+tap "homebrew/cask"
+cask_args appdir: "~/Applications", require_sha: true
+
+# Utils
+brew "bash-completion2"
+brew "bash"
+brew "coreutils"
+brew "git"
+brew "jq"
+
+# Languages
+brew "nvm"
+brew "pyenv-virtualenv"
+
+# Apps
+cask "1password"
+cask "alfred"
+cask "docker"
+cask "google-chrome"
+cask "postman"
+cask "slack"
+cask "spotify"
+cask "visual-studio-code"
--- brew.sh
+++ brew.sh
@@ -10,28 +10,11 @@ if ! [ -x "$(command -v brew)" ]; then
 fi
 
 brew update
-
 brew upgrade
-
-# CoreUtils
-brew install coreutils
-
-# Bash
-brew install bash bash-completion2
-
-# Utils
-brew install \
-    git \
-    jq
-
-# Languages
-brew install \
-    nvm \
-    pyenv-virtualenv
+brew bundle
+brew cleanup
 
 # rvm
 if ! [ -x "$(command -v rvm)" ]; then
     curl -sSL https://get.rvm.io | bash -s stable --ruby
 fi
-
-brew cleanup
